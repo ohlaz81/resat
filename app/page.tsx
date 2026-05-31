f52244642bcf2c1c00e2e6dd3efd49e0dac1c8a9
Murat Ohlaz <ohlaz81@gmail.com>
2026-05-31 21:52:27 +0300

    Header test

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Header from "../components/Header";
+
 export default function Home() {
   return (
-    <main className="min-h-screen flex items-center justify-center">
-      <h1>RESAT.NET TEST</h1>
+    <main className="min-h-screen bg-gray-100">
+      <Header />
     </main>
   );
 }
