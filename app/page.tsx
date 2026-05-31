d24039c9da644ebb33d7db5be7a9af2f1f5d744a
Murat Ohlaz <ohlaz81@gmail.com>
2026-05-31 21:57:21 +0300

    GoldCard test

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "../components/Header";
+import GoldCard from "../components/GoldCard";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-gray-100">
       <Header />
+      <GoldCard />
     </main>
   );
 }
