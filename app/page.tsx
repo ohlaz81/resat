78f1b7346eabc221bc96b8b367ca07cf1232f451
Murat Ohlaz <ohlaz81@gmail.com>
2026-05-31 21:43:12 +0300

    Ana sayfa test

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,7 @@
-import Header from "../components/Header";
-import GoldCard from "../components/GoldCard";
-import CurrencyCards from "../components/CurrencyCards";
-import GoldList from "../components/GoldList";
-import BottomNav from "../components/BottomNav";
-
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gray-100 pb-24">
-      <div className="max-w-md mx-auto p-4">
-        <Header />
-        <GoldCard />
-        <CurrencyCards />
-        <GoldList />
-        <BottomNav />
-      </div>
+    <main className="min-h-screen flex items-center justify-center">
+      <h1>RESAT.NET TEST</h1>
     </main>
   );
 }
