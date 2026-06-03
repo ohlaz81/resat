b142b20a0c05cd2f366c5fee2360987bbd3d0001
Murat Ohlaz <ohlaz81@gmail.com>
2026-06-03 20:37:40 +0300

    improve sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,67 +7,78 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: baseUrl,
       lastModified: new Date(),
+      changeFrequency: "hourly",
       priority: 1,
     },
 
     {
-      url: `${baseUrl}/haberler`,
+      url: `${baseUrl}/resat-altin`,
       lastModified: new Date(),
-      priority: 0.9,
+      changeFrequency: "hourly",
+      priority: 0.95,
     },
 
     {
       url: `${baseUrl}/gram-altin`,
       lastModified: new Date(),
+      changeFrequency: "hourly",
       priority: 0.9,
     },
 
     {
       url: `${baseUrl}/ceyrek-altin`,
       lastModified: new Date(),
+      changeFrequency: "hourly",
       priority: 0.9,
     },
 
     {
       url: `${baseUrl}/yarim-altin`,
       lastModified: new Date(),
+      changeFrequency: "hourly",
       priority: 0.9,
     },
 
     {
       url: `${baseUrl}/tam-altin`,
       lastModified: new Date(),
-      priority: 0.9,
-    },
-
-    {
-      url: `${baseUrl}/resat-altin`,
-      lastModified: new Date(),
+      changeFrequency: "hourly",
       priority: 0.9,
     },
 
     {
       url: `${baseUrl}/cumhuriyet-altini`,
       lastModified: new Date(),
+      changeFrequency: "hourly",
       priority: 0.9,
     },
 
     {
       url: `${baseUrl}/ons-altin`,
       lastModified: new Date(),
+      changeFrequency: "hourly",
       priority: 0.9,
     },
 
     {
       url: `${baseUrl}/grafikler`,
       lastModified: new Date(),
+      changeFrequency: "daily",
       priority: 0.8,
     },
 
     {
       url: `${baseUrl}/fiyat-alarmi`,
       lastModified: new Date(),
+      changeFrequency: "daily",
       priority: 0.8,
+    },
+
+    {
+      url: `${baseUrl}/gizlilik-politikasi`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
     },
   ];
 }
