6eb065f78c48a41e286ca626bd5d94afbdcd4652
Murat Ohlaz <ohlaz81@gmail.com>
2026-06-03 19:33:34 +0300

    update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,19 +1,71 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://resatnet.vercel.app";
+
   return [
     {
-      url: "https://resatnet.vercel.app",
+      url: baseUrl,
       lastModified: new Date(),
       priority: 1,
     },
+
     {
-      url: "https://resatnet.vercel.app/grafikler",
+      url: `${baseUrl}/haberler`,
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+
+    {
+      url: `${baseUrl}/gram-altin`,
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+
+    {
+      url: `${baseUrl}/ceyrek-altin`,
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+
+    {
+      url: `${baseUrl}/yarim-altin`,
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+
+    {
+      url: `${baseUrl}/tam-altin`,
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+
+    {
+      url: `${baseUrl}/resat-altin`,
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+
+    {
+      url: `${baseUrl}/cumhuriyet-altini`,
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+
+    {
+      url: `${baseUrl}/ons-altin`,
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+
+    {
+      url: `${baseUrl}/grafikler`,
       lastModified: new Date(),
       priority: 0.8,
     },
+
     {
-      url: "https://resatnet.vercel.app/fiyat-alarmi",
+      url: `${baseUrl}/fiyat-alarmi`,
       lastModified: new Date(),
       priority: 0.8,
     },
